5bb3440a0fc150461e127981749dbef372bf1f93
BaDuc311 <89256571+BaDuc311@users.noreply.github.com>
2021-09-23 00:29:27 +0700

    uploaod

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "Eman Sabir" && b=="27-06-1996")
+var b=prompt("Enter password");
+  	if((a== "Giang" || a=="Nguyễn Trà Giang" || a=="Trà Giang") && (b=="24/09/2005" ||b=="24-09-2005" ||b=="24092005"))
   	{
   	    window.location.href="start.html";
   	}
