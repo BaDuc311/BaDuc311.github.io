6faa29de511415e32678a77dd0801da3726ceac4
BaDuc311 <89256571+BaDuc311@users.noreply.github.com>
2021-09-23 20:39:26 +0700

    upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var b=prompt("Enter Password");
   	if((a== "Giang" || a=="Nguyễn Trà Giang" || a=="Trà Giang" || a=="giang" || a=="tra giang" || a=="Tra Giang" || a=="giangdepzai")
 	   && (b=="24/09/2005" ||b=="24-09-2005" ||b=="24092005" ||b=="2492005"))
   	{
-  	    window.location.href="Birthday.html";
+  	    window.location.href="start.html";
   	}
   	else
   	{
