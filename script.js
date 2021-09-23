aea4985789c54895fd7bd6dbfcd009736aa8809b
BaDuc311 <89256571+BaDuc311@users.noreply.github.com>
2021-09-23 20:37:16 +0700

    upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var a=prompt("Enter UserName");
-var b=prompt("Enter password");
+var a=prompt("Enter YourName");
+var b=prompt("Enter Password");
   	if((a== "Giang" || a=="Nguyễn Trà Giang" || a=="Trà Giang" || a=="giang" || a=="tra giang" || a=="Tra Giang" || a=="giangdepzai")
 	   && (b=="24/09/2005" ||b=="24-09-2005" ||b=="24092005" ||b=="2492005"))
   	{
-  	    window.location.href="start.html";
+  	    window.location.href="Birthday.html";
   	}
   	else
   	{
-  		alert("invalid details");
+  		alert("Nhập lại nào");
   		window.location.href="index.html";
   	}
   	
