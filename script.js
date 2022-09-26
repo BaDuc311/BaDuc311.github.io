1e35ecf2f49eb73703f2bf28191dbdd9aa8803dc
nguyenbaduc311 <85680843+nguyenbaduc311@users.noreply.github.com>
2022-09-26 17:33:13 +0700

    upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var a=prompt("Enter YourName");
 var b=prompt("Enter Password");
-  	if((a=="Nguyễn Trà Giang" || a=="Trà Giang" || a=="tragiang" || a=="Tra Giang" || a=="giangdepzai"||a=="nguyentragiang")
-	   && (b=="24/09/2005" ||b=="24-09-2005" ||b=="24092005" ||b=="2492005"||b=="24/9/2005"))
+  	if((a=="Nguyễn Trà Giang" || a=="Trà Giang" || a=="tragiang" || a=="Tra Giang" || a=="giangdepzai05"||a=="nguyentragiang" || a=="giang" || a=="Giang")
+	   && (b=="24/09/2005" ||b=="24-09-2005" ||b=="24092005" ||b=="2492005"||b=="24/9/2005"||b=="240905"))
   	{
   	    window.location.href="start.html";
   	}
   	else
   	{
-  		alert("=)))) thế cũng sai được luôn. Điền đầy đủ vào");
+  		alert("Điền như trước");
   		window.location.href="index.html";
   	}
   	
